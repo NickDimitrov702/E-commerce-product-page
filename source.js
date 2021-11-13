7dc9fe7f0e3bca89cd154256f072ca1341f90dcb
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2021-11-13 15:57:41 +0200

    Hoover effect on all, quantitiy counter.

---
+++ source.js
@@ -0,0 +1,36 @@
+// Functional Counter
+
+let countPlus = document.getElementById('+btn')
+let countMinus = document.getElementById('-btn')
+let totalCount = document.getElementById('input-counter')
+let counter = 0;
+
+updateDispaly()
+
+countPlus.addEventListener('click', () => {
+    counter++;
+    updateDispaly()
+})
+
+countMinus.addEventListener('click', () => {
+    
+    if(counter > 0) {
+        counter -= 1;
+    }
+
+    updateDispaly()
+})
+
+function updateDispaly () {
+    totalCount.value = counter
+}
+
+// Add to Cart Button
+
+let addToCartBtn = document.getElementById('add-to-cart-btn')
+
+
+
+addToCartBtn.addEventListener('click' ,() => {
+
+})
