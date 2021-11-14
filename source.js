4267719ee233242c6880fa81a3e216a8ab1a4128
Nickolay Dimitrov <dimitrov.nikolai9224@gmail.com>
2021-11-14 16:05:20 +0200

    Toogle functin for the cart icon to open and close the cart section, made the add-to-cart button work, by choosing the quantity you can sucssesfully add the item that will show in the cart with the correct quantity and the total amount. Made the delete icon work by deleting the item in the cart and showing the "Your cart is empty" messege

--- source.js
+++ source.js
@@ -3,13 +3,82 @@
 let countPlus = document.getElementById('+btn')
 let countMinus = document.getElementById('-btn')
 let totalCount = document.getElementById('input-counter')
+
+let cartIcon = document.getElementById('cart')
+let addToCartButton = document.getElementById('add-to-cart-btn')
+let cartItemQuantity = document.getElementById('cart-quantity-indicator')
+let checkOutCart = document.getElementById('checkout')
+
+checkOutCart.style.visibility = "hidden"
+cartItemQuantity.style.visibility = "hidden"
+
+let state = checkOutCart.style.visibility = "hidden"
+
 let counter = 0;
+
+let stateVisible = checkOutCart.style.visibility = "visible";
+let stateHidden = checkOutCart.style.visibility = "hidden";
+
+let checkOutImgItem = document.getElementById('check-out-image')
+let itemName = document.getElementsByClassName('check-out-item-name')
+let itemPrice = document.getElementById('item-price')
+let itemQuantity = document.getElementById('item-quantity')
+let itemTotalPrice = document.getElementById('item-total-price')
+
+let checkOutMessege = document.getElementById('empty-cart-messege')
+let checkOutBtn = document.getElementById('check-out-btn')
+let checkOutBody =  document.getElementById('check-out-cart-item-body')
+let checkOutItem = document.getElementById('check-out-item-wrapper')
+checkOutItem.parentNode.removeChild(checkOutItem)
+checkOutBtn.parentNode.removeChild(checkOutBtn)
+// checkOutBody.style.visibility = 'visible'
+
 
 updateDispaly()
 
 countPlus.addEventListener('click', () => {
     counter++;
     updateDispaly()
+
+
+    addToCartButton.addEventListener('click', () => {
+
+        
+        
+
+        if (counter > 0) {
+            
+            let totalPrice = 0;
+            let splitItemPrice = itemPrice.innerHTML
+            let thePriceSplit = splitItemPrice.split("$")
+            let itemPriceInt = parseInt(thePriceSplit[1])
+            cartItemQuantity.value = counter;
+            cartItemQuantity.style.visibility = 'visible'
+            itemQuantity.innerHTML = counter
+            totalPrice = counter * itemPriceInt
+            itemTotalPrice.innerHTML = `$${totalPrice.toFixed(2)}`
+
+            checkOutMessege.style.display = "none"
+            
+            checkOutBody.appendChild(checkOutItem)
+            checkOutBody.appendChild(checkOutBtn)
+            let deleteBtn = document.getElementById('delete');
+            
+
+            deleteBtn.addEventListener('click', () => {
+                checkOutItem.parentNode.removeChild(checkOutItem)
+                checkOutBtn.parentNode.removeChild(checkOutBtn)
+                checkOutMessege.style.display = ""
+                cartItemQuantity.style.visibility ="hidden"
+            })
+            
+        }
+
+        
+
+
+    })
+
 })
 
 countMinus.addEventListener('click', () => {
@@ -25,7 +94,32 @@ function updateDispaly() {
     totalCount.value = counter
 }
 
+function cartShower() {
+    state = checkOutCart.style.visibility = "visible"
+}
+
+function cartHider() {
+    state = checkOutCart.style.visibility = "hidden"
+}
+
+function toogleCart() {
+    if (state === stateHidden) {
+        checkOutCart.style.visibility = "visible";
+        cartShower()
+    } else {
+        checkOutCart.style.visibility = "hidden"
+        cartHider()
+    }
+}
+
+function itemSelect() {
+
+}
+
 // Add to Cart Button
+
+
+
 
 
 
